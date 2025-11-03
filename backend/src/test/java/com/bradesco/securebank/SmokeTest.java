4919127cd1a17f881274d341c1e387cab8a949df
Fillipe Ribeiro <rfillipe21@gmail.com>
2025-11-03 16:39:20 -0300

    Create SmokeTest.java

---
+++ backend/src/test/java/com/bradesco/securebank/SmokeTest.java
@@ -0,0 +1,6 @@
+package com.bradesco.securebank;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class SmokeTest {
+  @Test void ok(){ assertTrue(true); }
+}
