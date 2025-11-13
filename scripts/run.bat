3d8083dbe9d50bbdcf1e94555d4b63e9385e8586
Fillipe Ribeiro <130413172+fillipebro@users.noreply.github.com>
2025-11-13 20:26:55 -0300

    Delete scripts directory

--- scripts/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-set JAR=backend\target\securebank-0.1.0.jar
-if not exist "%JAR%" (
-  echo Jar nao encontrado em %JAR%
-  echo Baixe o artifact 'securebank-jar' do GitHub Actions e coloque em backend\target\
-  exit /b 1
-)
-java -jar "%JAR%"
